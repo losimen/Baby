9d2e0b670a35471b5b533c08e563db5276f30d0c
losimen <koval.pavlo03@gmail.com>
2023-04-29 13:41:16 +0300

    try print 0%

--- main.cpp
+++ main.cpp
@@ -27,12 +27,24 @@ int main() {
     processor_info_array_t processorInfo;
     mach_msg_type_number_t processorInfoCount;
 
-    task_info_t taskInfo;
+    task_info_data_t taskInfo;
     mach_msg_type_number_t taskInfoCount = TASK_INFO_MAX;
 
     for (int i = 0; i < numProcs; ++i) {
         std::cout << "Process name: " << procList[i].kp_proc.p_comm << std::endl;
         std::cout << "Process ID: " << procList[i].kp_proc.p_pid << std::endl;
+
+        pid_t pid = procList[i].kp_proc.p_pid;
+        int ret = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&taskInfo, &taskInfoCount);
+        if (ret != KERN_SUCCESS) {
+            std::cerr << "Failed to get basic task info for process " << pid << std::endl;
+            break;
+        }
+
+        double cpuUsage = ((double)taskInfo[0] / (double)TH_USAGE_SCALE) * 100.0;
+        std::cout << "CPU usage: " << cpuUsage << "%" << std::endl;
+
+        std::cout << std::endl;
     }
 
     return 0;
